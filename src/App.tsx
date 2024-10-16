7df220020778991fdb5933d6a597819eb02edaa0
Nariman <mirmanov1@mail.ru>
2024-10-17 03:33:50 +0500

    fix: refactor App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import "./App.css";
+
 import Header from "./components/Header/Header";
 import IntroSection from "./components/IntroSection/IntroSection";
 import IfYouSection from "./components/IfYouSection/IfYouSection";
@@ -6,9 +8,8 @@ import SubjectsSection from "./components/SubjectsSection/SubjectsSection";
 import FutureSection from "./components/FutureSection/FutureSection";
 import CourseSection from "./components/CourseSection/CourseSection";
 import ContactSection from "./components/ContactSection/ContactSection";
-
-import "./App.css";
 import Footer from "./components/Footer/Footer";
+
 function App() {
   return (
     <div className='app-container'>
